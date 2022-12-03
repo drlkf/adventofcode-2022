de3d728526f74a1bf9e3340ee05a1af7435d8d93
drlkf <drlkf@drlkf.net>
2022-12-03 22:03:24 +0100

    add day 3 part 2

---
+++ day-3/Part-2.hs
@@ -0,0 +1,34 @@
+module Main where
+
+import           Data.Char          (isUpper, ord, toLower)
+import           Data.List.Split    (chunksOf)
+import           System.Environment (getArgs)
+
+uniq :: (Eq a) => [a] -> [a]
+uniq [] = []
+uniq (a:as) | a `elem` as = uniq as
+            | otherwise = a:as
+
+duplicates :: (String, String, String) -> String
+duplicates (s1, s2, s3) = filter (`elem` s3) $ filter (`elem` s2) s1
+
+priority :: Char -> Int
+priority c | isUpper c = 26 + priority (toLower c)
+           | otherwise = ord c - ord 'a' + 1
+
+toGroup :: [String] -> (String, String, String)
+toGroup [s1, s2, s3] = (s1, s2, s3)
+toGroup ls           = error $ "incomplete input: " ++ show ls
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let readInput = if null args
+                  then getContents
+                  else readFile $ head args
+
+  cts <- readInput
+  let groups = map toGroup $ chunksOf 3 $ lines cts
+      badges = map (uniq . duplicates) groups
+
+  print $ sum $ map priority $ concat badges
