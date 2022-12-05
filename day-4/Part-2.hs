084d9261151251f68d5f0fb7691aed0b98220e30
drlkf <drlkf@drlkf.net>
2022-12-05 17:04:10 +0100

    add day 4 part 2

---
+++ day-4/Part-2.hs
@@ -0,0 +1,52 @@
+module Main where
+
+import           Data.Char                       (isDigit)
+import           System.Environment              (getArgs)
+import           Text.ParserCombinators.ReadP    (many, satisfy)
+import           Text.ParserCombinators.ReadPrec (get, lift, pfail)
+import           Text.Read                       (readPrec)
+
+data Assignment = Assignment Int Int
+  deriving Show
+
+instance Read Assignment where
+  readPrec = do
+    x <- lift $ many $ satisfy isDigit
+    get >>= dash
+    y <- lift $ many $ satisfy isDigit
+    return $ Assignment (read x) (read y)
+      where dash '-' = return ()
+            dash  _  = pfail
+
+data ElfPair = ElfPair Assignment Assignment
+  deriving Show
+
+instance Read ElfPair where
+  readPrec = do
+    left <- readPrec
+    get >>= comma
+    right <- readPrec
+    return $ ElfPair left right
+    where comma ',' = return ()
+          comma  _  = pfail
+
+toRange :: Assignment -> [Int]
+toRange (Assignment x y) = [x..y]
+
+hasOverlap :: Assignment -> Assignment -> Bool
+hasOverlap a b = any (`elem` toRange b) $ toRange a
+
+overlaps :: ElfPair -> Bool
+overlaps (ElfPair a b) = hasOverlap a b || hasOverlap b a
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let readInput = if null args
+                  then getContents
+                  else readFile $ head args
+
+  cts <- readInput
+  let pairs = map read $ lines cts
+
+  print $ length $ filter id $ map overlaps pairs
