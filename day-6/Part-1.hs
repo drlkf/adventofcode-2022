1044492dc4008da932c9c750e609aed02017f4ca
drlkf <drlkf@drlkf.net>
2022-12-06 18:53:03 +0100

    add day 6 part 2

--- day-6/Part-1.hs
+++ day-6/Part-1.hs
@@ -3,12 +3,16 @@ module Main where
 import           Data.List          (nub)
 import           System.Environment (getArgs)
 
+prefixLength :: Int
+prefixLength = 4 -- part 1
+-- prefixLength = 14 -- part 2
+
 marker :: String -> Maybe String
 marker s
-  | length prefix < 4        = Nothing
-  | length (nub prefix) == 4 = Just prefix
-  | otherwise                = (c :) <$> marker cs
-  where prefix = take 4 s
+  | length prefix < prefixLength        = Nothing
+  | length (nub prefix) == prefixLength = Just prefix
+  | otherwise                           = (c :) <$> marker cs
+  where prefix = take prefixLength s
         c = head s
         cs = tail s
 
